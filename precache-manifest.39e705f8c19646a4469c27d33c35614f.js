c98f9578830d0ef4efa0a2f1c643ee88f1d5c1d8
Clément Acher <clement.acher@gmail.com>
2019-03-05 19:43:57 +0100

    Updates

---
+++ precache-manifest.39e705f8c19646a4469c27d33c35614f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cafb7fc51c5f4e075697",
+    "url": "/blindtest-spotify/static/css/main.d4124f39.chunk.css"
+  },
+  {
+    "revision": "cafb7fc51c5f4e075697",
+    "url": "/blindtest-spotify/static/js/main.cafb7fc5.chunk.js"
+  },
+  {
+    "revision": "2bed69904f22342c89d6",
+    "url": "/blindtest-spotify/static/js/runtime~main.2bed6990.js"
+  },
+  {
+    "revision": "5d9107c347decab20cac",
+    "url": "/blindtest-spotify/static/js/2.5d9107c3.chunk.js"
+  },
+  {
+    "revision": "79f6b6a0c4f3cca321dc458fe7e6544a",
+    "url": "/blindtest-spotify/static/media/logo.79f6b6a0.svg"
+  },
+  {
+    "revision": "9d4cbf40dcdac09bda6fa0aff397246b",
+    "url": "/blindtest-spotify/static/media/loading.9d4cbf40.svg"
+  },
+  {
+    "revision": "0fa93abb9bde15357c5e62f241df4914",
+    "url": "/blindtest-spotify/index.html"
+  }
+];
